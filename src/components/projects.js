2f3350243f7566ca86f5bb67cbf39aad9fa82d6e
aflanders7 <audrey.flanders117@gmail.com>
2023-08-23 17:13:24 -0400

    image src

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,7 @@
 // projects with a live page
 export const Projects = [
     {
-        image: 'images/Bookstore.png',
+        image: './images/Bookstore.png',
         title: 'Bookstore Database',
         technology: 'MySQL, Node.JS, Express, Handlebars, JavaScript',
         description: 'Database management system for a bookstore. Built with Node.JS and MySQL for backend functionality. Uses CRUD functions to manage and track product inventory, customers, employees, and sales. Deployed with Oracle Cloud.',
@@ -13,7 +13,7 @@ export const Projects = [
 // projects without a live page
 export const Projects2 = [
     {
-        image: 'images/hashmap.png',
+        image: './images/hashmap.png',
         title: 'HashMap Implementation',
         technology: 'Python',
         description: 'HashMaps implemented with underlying dynamic arrays. OA HashMap collisions handled with open addressing via quadratic probing. SC HashMap collisions resolved with chaining via singly linked lists.',
@@ -21,7 +21,7 @@ export const Projects2 = [
         code: "Code"
     },
     {
-        image: 'images/taskmap.png',
+        image: './images/taskmap.png',
         title: 'Construction Progress Tracker',
         technology: 'Next.JS, TypeScript, Tailwind CSS, DaisyUI',
         description: 'Frontend human-centered design for a construction tracker website owned by Wayfinder Data LLC. Created views and tables for analyzing construction progress and worker efficiency. Worked directly with the client to understand and meet their needs. Code unavailable due to ownership rights.',
@@ -29,7 +29,7 @@ export const Projects2 = [
         code: "GitHub"
     },
     {
-        image: 'images/mentalhealth.png',
+        image: './images/mentalhealth.png',
         title: 'Mental Health Website',
         technology: 'Svelte, JavaScript',
         description: 'Mental health website featuring a chatbot and breathing animation. Designed and built with a team during a Spring 2023 hackathon. My contributions included the frontend design, JavaScript animation, and integrating the chatbot.',
