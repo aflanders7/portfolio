71a4039ac11b7bc697e0e9127e1759e6bfb99fcf
aflanders7 <audrey.flanders117@gmail.com>
2023-08-22 23:29:01 -0400

    project component

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,27 +1,6 @@
 import React from 'react';
-import { BsGithub } from 'react-icons/bs'; 
-import { DiGithubFull } from 'react-icons/di'; 
+import { Projects, Projects2 } from '../components/projects';
 
-const Projects = [
-    {
-        image: 'images/Bookstore.png',
-        title: 'Bookstore Database',
-        technology: 'MySQL, Node.JS, Express, Handlebars, JavaScript',
-        description: 'Database management system for a bookstore. Built with Node.JS and MySQL for backend functionality. Uses CRUD functions to manage and track product inventory, customers, employees, and sales. Deployed with Oracle Cloud.',
-        github: 'https://github.com/aflanders7/Bookstore',
-        live: 'http://bookstore.ddns.net:8034/',
-    }
-];
-
-const Projects2 = [
-    {
-        image: 'images/mentalhealth.png',
-        title: 'Mental Health Website',
-        technology: 'Svelte, JavaScript',
-        description: 'Mental health website featuring a chatbot and breathing animation. Designed and built with a team during a Spring 2023 hackathon. My contributions included the frontend design, JavaScript animation, and integrating the chatbot.',
-        github: 'https://github.com/aflanders7/MentalHealthApp-BeaverHack2023',
-    }
-];
 
 function ProjectsPage() {
     const data = Projects;
